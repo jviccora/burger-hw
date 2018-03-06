2d16f408277419208659ea33c93e3ea86cdb9884
Mike Ignaczak <mikeignaczak@gmail.com>
2018-03-05 21:46:32 -0600

    Pretty Much Done

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -27,7 +27,7 @@ $(function() {
 
     var newBurger = {
       burger_name: $("#br").val().trim(),
-      devoured: $("[name=devoured]:checked").val().trim()
+      devoured: $("[name=devoured]:checked").val()
     };
 
     // Send the POST request.
